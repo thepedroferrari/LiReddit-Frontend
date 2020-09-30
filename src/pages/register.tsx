2686983840d5097ded43dd8ce15bb2bb2c96137e
thepedroferrari <pedro@pedroferrari.com>
2020-09-30 02:01:09 +0200

    Install formik and copy example from Chakra

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,10 +1,43 @@
+import { Button, FormControl, FormErrorMessage, FormLabel, Input } from '@chakra-ui/core';
+import {Formik, Field} from 'formik'
 interface registerProps {
 
 }
 
+
 const Register: React.FC<registerProps> = ({}) => {
   return (
-      <div>Register Page</div>
+    <Formik
+      initialValues={{ name: "Sasuke" }}
+      onSubmit={(values, actions) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          actions.setSubmitting(false);
+        }, 1000);
+      }}
+    >
+      {props => (
+        <form onSubmit={props.handleSubmit}>
+          <Field name="name">
+            {({ field, form }) => (
+              <FormControl isInvalid={form.errors.name && form.touched.name}>
+                <FormLabel htmlFor="name">First name</FormLabel>
+                <Input {...field} id="name" placeholder="name" />
+                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+              </FormControl>
+            )}
+          </Field>
+          <Button
+            mt={4}
+            variantColor="teal"
+            isLoading={props.isSubmitting}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+      )}
+    </Formik>
     );
 }
 
