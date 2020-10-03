93d0c941b76abebd5bc7f5a4dfe2d7a299cff7ce
thepedroferrari <pedro@pedroferrari.com>
2020-10-03 15:06:22 +0200

    Cache exclusion when login/Register

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,39 @@
-import { Box, Flex, Link } from '@chakra-ui/core';
+import { Box, Button, Flex, Link } from '@chakra-ui/core';
 import NextLink from 'next/link';
+import { useMeQuery } from '../generated/graphql';
 
 export const NavBar: React.FC = () => {
+  const [{ data, fetching }] = useMeQuery()
+  let body = null;
+
+  // loading
+  fetching
+    ? body = <p>Loading...</p>
+    : !data?.me
+      ? body = (
+        <>
+          <NextLink href="/login">
+            <Link mr={2}>Login</Link>
+          </NextLink>
+          <NextLink href="/register">
+            <Link mr={2}>Register</Link>
+          </NextLink>
+        </>
+      )
+      : body = (
+        <Flex>
+          <Box>{data.me.username}</Box>
+          <Button variant="link">Logout</Button>
+        </Flex>
+      );
+
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex bg="#ffd500" p={4}>
       <NextLink href="/">
         <Link mr={2}>Logo</Link>
       </NextLink>
       <Box ml={"auto"}>
-        <NextLink href="/login">
-          <Link mr={2}>Login</Link>
-        </NextLink>
-        <NextLink href="/register">
-          <Link mr={2}>Register</Link>
-        </NextLink>
+        {body}
       </Box>
     </Flex>
   );
