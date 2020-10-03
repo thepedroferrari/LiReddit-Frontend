02f8ef7a5b956fc6dc9c75efe1a128cd3bb994a0
thepedroferrari <pedro@pedroferrari.com>
2020-10-03 14:13:14 +0200

    Strict TS

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { Box } from '@chakra-ui/core';
+import React from 'react'
+
+interface NavBarProps {
+
+}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  return (
+    <Box bg="tomato">
+      tomatoes are good
+    </Box>
+  );
+}
