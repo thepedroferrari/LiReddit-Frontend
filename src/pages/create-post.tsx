8f217cd1fa14597e843c04f2a7319af50600579c
thepedroferrari <pedro@pedroferrari.com>
2020-10-11 20:13:52 +0200

    6:09:00 Create Post Page

---
+++ src/pages/create-post.tsx
@@ -0,0 +1,56 @@
+import { Box, Button } from '@chakra-ui/core';
+import { Formik, Form } from 'formik';
+import { useRouter } from 'next/router';
+
+import { InputField } from '../components/InputField';
+import { Wrapper } from '../components/Wrapper';
+import { useCreatePostMutation } from '../generated/graphql';
+import { withUrqlClient } from 'next-urql';
+import { createUrqlClient } from '../utils/createUrqlClient';
+
+const CreatePost: React.FC = () => {
+  const router = useRouter();
+  const [, createPost] = useCreatePostMutation();
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ title: "", text: "" }}
+        onSubmit={async (input) => {
+          await createPost({ input });
+          router.push('/')
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="title"
+              placeholder="Post Title"
+              label="Post Title"
+              autoComplete=''
+            />
+            <Box mt={4}>
+              <InputField
+                name="text"
+                placeholder="content..."
+                label="Post content"
+                type="text"
+                textarea
+              />
+            </Box>
+
+            <Button
+              mt={4}
+              variantColor="green"
+              isLoading={isSubmitting}
+              type="submit"
+            >
+              Create Post
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  )
+}
+
+export default withUrqlClient(createUrqlClient)(CreatePost)
