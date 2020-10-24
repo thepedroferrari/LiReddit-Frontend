b8c72d977fd9c3cb68721d49b0044e28ce203876
thepedroferrari <pedro@pedroferrari.com>
2020-10-24 21:47:58 +0200

    8:01:20 Fix pagination

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,65 @@
-import { Box, Button, Flex, Heading, Stack, Text } from '@chakra-ui/core';
 import { withUrqlClient } from "next-urql";
-import NextLink from 'next/link';
-import { useCallback, useState } from 'react';
-
-import { Layout } from '../components/Layout';
-import { usePostsQuery } from '../generated/graphql';
-import { createUrqlClient } from '../utils/createUrqlClient';
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
+import { Layout } from "../components/Layout";
+import { Link, Stack, Box, Heading, Text, Flex, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useState } from "react";
 
 const Index = () => {
   const [variables, setVariables] = useState({
     limit: 10,
-    cursor: null as null | string
-  })
+    cursor: null as null | string,
+  });
 
-  const [{ data, fetching }] = usePostsQuery({ variables });
+  const [{ data, fetching }] = usePostsQuery({
+    variables,
+  });
 
-  const handlePagination = useCallback(() => {
-    if (!data) {
-      console.log('not data')
-      return;
-    }
-
-    setVariables({
-      limit: variables.limit,
-      cursor: data.posts.posts[data?.posts.posts.length - 1].createdAt
-    })
-  }, [])
+  if (!fetching && !data) {
+    return <div>you got query failed for some reason</div>;
+  }
 
   return (
     <Layout>
-      <Flex align="center" mb={8}>
+      <Flex align="center">
         <Heading>LiReddit</Heading>
         <NextLink href="/create-post">
-          <Button background="#77cc22" ml="auto">Create Post</Button>
+          <Link ml="auto">create post</Link>
         </NextLink>
       </Flex>
-      {data && !fetching
-        ? (
+      <br />
+      {!data && fetching ? (
+        <div>loading...</div>
+      ) : (
           <Stack spacing={8}>
-            {data!.posts.posts.map(post => (
-              <header key={post.id}>
-                <Box p={5} shadow="md" borderWidth="1px" mb={8}>
-                  <Heading fontSize="xl">{post.title}</Heading>
-                  <Text mt={4}>{post.excerpt}...</Text>
-                </Box>
-              </header>
+            {data!.posts.posts.map((p) => (
+              <Box key={p.id} p={5} shadow="md" borderWidth="1px">
+                <Heading fontSize="xl">{p.title}</Heading>
+                <Text mt={4}>{p.excerpt}</Text>
+              </Box>
             ))}
           </Stack>
-        ) : <p>loading...</p>
-      }
-      {data && data.posts.hasMore && (
+        )}
+      {data && data.posts.hasMore ? (
         <Flex>
-          <Button m="auto"
-            onClick={handlePagination}
+          <Button
+            onClick={() => {
+              setVariables({
+                limit: variables.limit,
+                cursor: data.posts.posts[data.posts.posts.length - 1].createdAt,
+              });
+            }}
             isLoading={fetching}
+            m="auto"
+            my={8}
           >
-            Load more
+            load more
           </Button>
         </Flex>
-      )}
+      ) : null}
     </Layout>
-  )
-}
+  );
+};
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index)
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
